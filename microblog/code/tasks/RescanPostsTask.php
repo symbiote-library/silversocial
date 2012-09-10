96e8f9948538e68ff54f505d766ed4b4e6f24683
Marcus Nyeholt <marcus@silverstripe.com.au>
2012-09-10 21:55:21 +1000

    Added task for reprocessing posts for spam

---
+++ microblog/code/tasks/RescanPostsTask.php
@@ -0,0 +1,25 @@
+e<?php
+
+/**
+ * @author marcus@silverstripe.com.au
+ * @license BSD License http://silverstripe.org/bsd-license/
+ */
+class RescanPostsTask extends BuildTask {
+	public function run($request) {
+		$id = (int) $request->getVar('id');
+		
+		if ($id) {
+			$post = DataList::create('MicroPost')->byID($id);
+			if ($post) {
+				singleton('QueuedJobService')->queueJob(new ProcessPostJob($post));
+			}
+		} else if ($request->getVar('all')) {
+			$posts = DataList::create('MicroPost');
+			foreach ($posts as $post) {
+				if ($post->Content != '[spam]') {
+					singleton('QueuedJobService')->queueJob(new ProcessPostJob($post));
+				}
+			}
+		}
+	}
+}
