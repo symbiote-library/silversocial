04b3c6d618a30b70a33b5b71cbd1abba3ff4aa17
Marcus Nyeholt <marcus@silverstripe.com.au>
2012-09-13 18:23:17 +1000

    Updated post processor to work under better combination of statistics

--- microblog/code/jobs/ProcessPostJob.php
+++ microblog/code/jobs/ProcessPostJob.php
@@ -47,43 +47,45 @@ class ProcessPostJob extends AbstractQueuedJob {
 	
 	public function process() {
 		$post = $this->getObject();
-		$author = $post->Owner();
-		$balance = $author->Balance;
+		if ($post) {
+			$author = $post->Owner();
+			$balance = $author->Balance;
 
-		if (self::$api_key && $balance < self::USER_THRESHOLD && $post->Content != self::SPAM_CONTENT) {
-			require_once Director::baseFolder() . '/microblog/thirdparty/defensio/Defensio.php';
-			$defensio = new Defensio(self::$api_key);
-			$document = array(
-				'type' => 'comment', 
-				'content' => $post->Content, 
-				'platform' => 'silverstripe_microblog', 
-				'client' => 'MicroBlog Defensio-PHP | 0.1 | Marcus Nyeholt | marcus@silverstripe.com.au', 
-				'async' => 'false'
-			);
+			if (self::$api_key && $balance < self::USER_THRESHOLD && $post->Content != self::SPAM_CONTENT) {
+				require_once Director::baseFolder() . '/microblog/thirdparty/defensio/Defensio.php';
+				$defensio = new Defensio(self::$api_key);
+				$document = array(
+					'type' => 'comment', 
+					'content' => $post->Content, 
+					'platform' => 'silverstripe_microblog', 
+					'client' => 'MicroBlog Defensio-PHP | 0.1 | Marcus Nyeholt | marcus@silverstripe.com.au', 
+					'async' => 'false'
+				);
 
-			try {
-				$result = $defensio->postDocument($document);
+				try {
+					$result = $defensio->postDocument($document);
 
-				if ($result && isset($result[1])) {
-					if ($result[1]->allow == 'false') {
-						$post->Content = self::SPAM_CONTENT;
-						$post->Down += self::SPAM_DOWN;
-						$post->write();
-						$author->Down += self::SPAM_DOWN;
-						$author->write();
+					if ($result && isset($result[1])) {
+						if ($result[1]->allow == 'false') {
+							$post->Content = self::SPAM_CONTENT;
+							$post->Down += self::SPAM_DOWN;
+							$post->write();
+							$author->Down += self::SPAM_DOWN;
+							$author->write();
+						}
 					}
+				} catch (Exception $e) {
+					SS_Log::log($e, SS_Log::WARN);
 				}
-			} catch (Exception $e) {
-				SS_Log::log($e, SS_Log::WARN);
 			}
-		}
-		
-		/**
-		 *Only convert post content by authors who should 
-		 */
-		if ($balance > self::USER_THRESHOLD) {
-			$this->socialGraphService->convertPostContent($post);
-			$post->write();
+
+			/**
+			*Only convert post content by authors who should 
+			*/
+			if ($balance > self::USER_THRESHOLD) {
+				$this->socialGraphService->convertPostContent($post);
+				$post->write();
+			}
 		}
 
 		$this->isComplete = true;
